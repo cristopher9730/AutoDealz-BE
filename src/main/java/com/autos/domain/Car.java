3793b4f21776530a7eb62952e30ccf7a0c8543b8
Cristopher Salazar <csalazara@ucenfotec.ac.cr>
2023-02-18 19:55:49 -0600

    Creating the car class

---
+++ src/main/java/com/autos/domain/Car.java
@@ -0,0 +1,22 @@
+package com.autos.domain;
+
+import lombok.Data;
+
+@Data
+public class Car {
+
+    private String id;
+    private String plateNumber;
+    private String color;
+    private String brand;
+    private String type;
+    private String year;
+    private double price;
+    private String picture;
+    private String fuel;
+    private String odometer;
+    private String transmission;
+    private String cylinders;
+
+
+}
