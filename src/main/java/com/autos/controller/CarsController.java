1e3acd464dce0b2375990e4245206af39e039bc9
Cristopher Salazar <csalazara@ucenfotec.ac.cr>
2023-03-04 18:35:48 -0600

    updateCar

--- src/main/java/com/autos/controller/CarsController.java
+++ src/main/java/com/autos/controller/CarsController.java
@@ -3,6 +3,7 @@ package com.autos.controller;
 import com.autos.domain.Car;
 import com.autos.service.CarService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +18,26 @@ import java.util.List;
 public class CarsController {
     @Autowired
     private CarService carService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping("/getCars")
     public List<Car> getCars(){
         return carService.get();
     }
 
-    @PostMapping(value="/submitCar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value="/updateCarMultipart", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String submitCar(@RequestPart("file") MultipartFile file, @RequestPart("car") String car) throws IOException {
         carService.processCar(file,car);
         return "Success";
     }
+
+    @PostMapping(value="/updateCar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String updateCar(@RequestPart("car") String car) throws JsonProcessingException {
+        carService.upsert(objectMapper.readValue(car,Car.class));
+        return "Success";
+    }
+
     @GetMapping("/deleteCar")
     public String deleteCar(@RequestParam String carId){
         carService.delete(carId);
--- src/main/java/com/autos/service/CarService.java
+++ src/main/java/com/autos/service/CarService.java
@@ -3,7 +3,7 @@ package com.autos.service;
 import com.autos.AppProperties;
 import com.autos.domain.Car;
 import com.autos.repository.CarRepository;
-import com.azure.spring.cloud.core.service.AzureServiceType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -12,6 +12,7 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,19 +36,26 @@ public class CarService {
     public void processCar(MultipartFile file, String carString) throws IOException {
         try{
             Car car = objectMapper.readValue(carString,Car.class);
-            MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
-            File tempFile =  new File(String.format("%s\\%s-%s",appProperties.getTempFolder(),System.currentTimeMillis(),file.getOriginalFilename()));
-            Files.write(tempFile.toPath(),file.getBytes());
-            entityBuilder.addBinaryBody("file",tempFile,ContentType.DEFAULT_BINARY,LocalDateTime.now().toString());
-            entityBuilder.addPart("upload_preset",new StringBody(appProperties.getUploadPreset()));
-            HttpClient client = new DefaultHttpClient();
-            HttpPost request = new HttpPost(appProperties.getCloudinaryUploadUrl());
-            request.setEntity(entityBuilder.build());
-            HttpResponse response = client.execute(request);
-            tempFile.delete();
+            car.setPicture(getCloudinaryURL(file));
+            upsert(car);
         }catch (Exception e){
             throw e;
         }
+    }
+
+    private String getCloudinaryURL(MultipartFile file) throws IOException {
+        MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+        File tempFile =  new File(String.format("%s\\%s-%s",appProperties.getTempFolder(),System.currentTimeMillis(), file.getOriginalFilename()));
+        Files.write(tempFile.toPath(), file.getBytes());
+        entityBuilder.addBinaryBody("file",tempFile,ContentType.DEFAULT_BINARY,LocalDateTime.now().toString());
+        entityBuilder.addPart("upload_preset",new StringBody(appProperties.getUploadPreset()));
+        HttpClient client = new DefaultHttpClient();
+        HttpPost request = new HttpPost(appProperties.getCloudinaryUploadUrl());
+        request.setEntity(entityBuilder.build());
+        HttpResponse response = client.execute(request);
+        tempFile.delete();
+        String url = objectMapper.readValue(EntityUtils.toString(response.getEntity(),"UTF-8"), JsonNode.class).get("secure_url").toString();
+        return url.substring(1,url.length()-1);
     }
 
     public void upsert(Car car){
